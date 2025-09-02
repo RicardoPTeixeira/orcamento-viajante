b5d92969ad6655e7b53db2a9c2b8ed1ce14c9075
RicardoPTeixeira <libra1ponciano@gmail.com>
2025-09-02 19:01:16 -0300

    feature: config redirect in home to login or chose travel

--- orcamento-viajante/src/pages/Home/index.jsx
+++ orcamento-viajante/src/pages/Home/index.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { auth } from '../../firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
 function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true); // Usuário está logado
+        window.location.href ='/escolha-viagem'
+      } else {
+        setIsAuthenticated(false); // Usuário não está logado
+        window.location.href ='/login'
+      }
+    });
+
+
+    return () => unsubscribe(); // Limpa o listener ao desmontar
+  }, []);
 
   return (
     <>
-      Home
+      {isAuthenticated ? "true" : "false"}
     </>
   )
 }
